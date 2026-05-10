1f583159f6075421d653cdeb59c248dce4a50415
Ali <ali123@gmail.com>
2026-05-10 17:14:31 +0300

    Implement templates

---
+++ build/array_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/Alishka/cpp/lab_templates/build/array_tests[1]_tests.cmake")
+  include("/home/Alishka/cpp/lab_templates/build/array_tests[1]_tests.cmake")
+else()
+  add_test(array_tests_NOT_BUILT array_tests_NOT_BUILT)
+endif()
